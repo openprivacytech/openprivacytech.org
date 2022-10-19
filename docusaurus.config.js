f43d5e2227f572c15cd8058a0835d2b4c3163a23
Sourav Das <souravrhythm@gmail.com>
2022-10-19 12:36:47 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Open Privacy Tech", // Usually your GitHub org/user name.
+  organizationName: "OpenPrivacyTech", // Usually your GitHub org/user name.
   projectName: "openprivacytech", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
