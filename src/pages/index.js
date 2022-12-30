eb8438c40e1addf3f7a0683fbb67c71ae01f8a6d
Sourav Das <souravrhythm@gmail.com>
2022-12-30 19:35:15 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
-              to="https://www.openprivacytech.org/docs/intro"
+              to="https://www.openprivacytech.org/docs"
             >
               GET STARTED
             </Link>
